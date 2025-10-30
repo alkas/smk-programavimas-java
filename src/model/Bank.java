225494bb70574cf87b1ace345fec8e7c63a6fb96
alkas <erikas.stanys@gmail.com>
2025-10-30 12:36:03 +0200

    Add Bank class with name property ant getters/setters

---
+++ src/model/Bank.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Bank {
+    private String name;
+
+    public Bank(String name) {
+        this.name = name;
+    }
+
+    // Getters
+    public String getName() { return name; }
+
+    // Setters
+    public void setName(String name) { this.name = name; }
+}
