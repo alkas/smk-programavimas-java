b346b3b808741d61d57ba8c001b2b2363a0b3c3c
alkas <erikas.stanys@gmail.com>
2025-10-30 18:45:37 +0200

    Modified main class after abstract class creation

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-import model.ATM;
-import model.Account;
-import model.Bank;
+import model.*;
 
 public class Main {
     public static void main(String[] args) {
         Bank seb = new Bank("SEB");
 
-        Account account1 = new Account("0123", "LT120252584660023", 0);
-        Account account2 = new Account("0124", "LT120252584660024", 125.30);
-        Account account3 = new Account("0125", "LT120252584660025", 500.00);
-        Account account4 = new Account("0126", "LT120252584660026", 75.50);
+        Account account1 = new PreferentialAccount("0123", "LT120252584660023", 0);
+        Account account2 = new RegularAccount("0124", "LT120252584660024", 125.30);
+        Account account3 = new PreferentialAccount("0125", "LT120252584660025", 500.00);
+        Account account4 = new RegularAccount("0126", "LT120252584660026", 75.50);
 
         ATM atm1 = new ATM("atm1", "Taikos pr.61, Klaipėda");
         ATM atm2 = new ATM("atm2", "Ozo g. 25, Vilnius");
